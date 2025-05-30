56d80192e61b4ee21c0a3a6b70b4d684781f993e
Gray Boulware <gray.boulware@gmail.com>
2025-05-30 12:37:14 -0500

    Consolidates context for view_content.html from browse, movies, & tv with _get_context() function

--- src/djangoflix/views.py
+++ src/djangoflix/views.py
@@ -115,6 +115,74 @@ def browse(request):
             return redirect(reverse_lazy("accounts:logout"))
         
     all_content = WatchableContent.get_all_content()
+    context = _get_context(all_content)
+    
+    
+    return render(request, "djangoflix/view_content.html", context)
+
+
+def movies(request):
+    if not request.method == "GET":
+        return HttpResponseNotAllowed(["GET"])
+    try:
+        this_profile = Profile.get_one_profile_by_id(request.session["profile"])
+        if not this_profile:
+            raise Profile.DoesNotExist
+    # KeyError catches login skippers, DoesNotExist catches invalid session data
+    except (KeyError, Profile.DoesNotExist):
+        if not "account" in request.session:
+            # Send them to the login page, not register
+            # If they are trying to get to home, they probably have an account
+            return redirect(reverse_lazy("accounts:login"))
+        elif not "profile" in request.session:
+            return redirect(reverse_lazy("djangoflix:profiles"))
+        else:
+            return redirect(reverse_lazy("accounts:logout"))
+        
+    all_content = WatchableContent.get_all_movies()
+    context = _get_context(all_content)
+    
+    return render(request, "djangoflix/view_content.html", context)
+
+
+def tv(request):
+    if not request.method == "GET":
+        return HttpResponseNotAllowed(["GET"])
+    try:
+        this_profile = Profile.get_one_profile_by_id(request.session["profile"])
+        if not this_profile:
+            raise Profile.DoesNotExist
+    # KeyError catches login skippers, DoesNotExist catches invalid session data
+    except (KeyError, Profile.DoesNotExist):
+        if not "account" in request.session:
+            # Send them to the login page, not register
+            # If they are trying to get to home, they probably have an account
+            return redirect(reverse_lazy("accounts:login"))
+        elif not "profile" in request.session:
+            return redirect(reverse_lazy("djangoflix:profiles"))
+        else:
+            return redirect(reverse_lazy("accounts:logout"))
+        
+    all_content = WatchableContent.get_all_tv()
+    context = _get_context(all_content)
+    
+    return render(request, "djangoflix/view_content.html", context)
+
+
+def favorites(request):
+    return HttpResponse("Hello, Favorites!")
+
+
+def search(request):
+    return HttpResponse("Hello, search!")
+
+
+def details(request):
+    return HttpResponse("Hello, Details!")
+
+
+### Consolidation function for browse, movies, & tv
+def _get_context(all_content) -> dict:
     action = []
     adventure = []
     animated = []
@@ -127,7 +195,6 @@ def browse(request):
     history = []
     horror = []
     kids = []
-    music = []
     mystery = []
     reality = []
     romance = []
@@ -168,8 +235,6 @@ def browse(request):
                     horror.append(content)
                 case "Kids":
                     kids.append(content)
-                case "Music":
-                    music.append(content)
                 case "Mystery":
                     mystery.append(content)
                 case "Reality":
@@ -185,8 +250,6 @@ def browse(request):
                     thriller.append(content)
                 case "TV Movie":
                     tv_movie.append(content)
-                case "Music":
-                    music.append(content)
                 case "War":
                     war.append(content)
                 case "War & Politics":
@@ -209,7 +272,6 @@ def browse(request):
         "history": history,
         "horror": horror,
         "kids": kids,
-        "music": music,
         "mystery": mystery,
         "reality": reality,
         "romance": romance,
@@ -218,265 +280,5 @@ def browse(request):
         "tv_movie": tv_movie,
         "war": war,
     }
-    
-    return render(request, "djangoflix/view_content.html", context)
 
-
-def movies(request):
-    if not request.method == "GET":
-        return HttpResponseNotAllowed(["GET"])
-    try:
-        this_profile = Profile.get_one_profile_by_id(request.session["profile"])
-        if not this_profile:
-            raise Profile.DoesNotExist
-    # KeyError catches login skippers, DoesNotExist catches invalid session data
-    except (KeyError, Profile.DoesNotExist):
-        if not "account" in request.session:
-            # Send them to the login page, not register
-            # If they are trying to get to home, they probably have an account
-            return redirect(reverse_lazy("accounts:login"))
-        elif not "profile" in request.session:
-            return redirect(reverse_lazy("djangoflix:profiles"))
-        else:
-            return redirect(reverse_lazy("accounts:logout"))
-        
-    all_movies = WatchableContent.get_all_movies()
-    action = []
-    adventure = []
-    animated = []
-    comedy = []
-    crime = []
-    documentary = []
-    drama = []
-    family = []
-    fantasy = []
-    history = []
-    horror = []
-    kids = []
-    music = []
-    mystery = []
-    reality = []
-    romance = []
-    scifi = []
-    thriller = []
-    tv_movie = []
-    war = []
-    western = []
-
-    for movie in all_movies:
-        genres = movie.genres.all()
-        for genre in genres:
-            match genre.name:
-                case "Action":
-                    action.append(movie)
-                case "Action & Adventure":
-                    action.append(movie)
-                    adventure.append(movie)
-                case "Adventure":
-                    adventure.append(movie)
-                case "Animation":
-                    animated.append(movie)
-                case "Comedy":
-                    comedy.append(movie)
-                case "Crime":
-                    crime.append(movie)
-                case "Documentary":
-                    documentary.append(movie)
-                case "Drama":
-                    drama.append(movie)
-                case "Family":
-                    family.append(movie)
-                case "Fantasy":
-                    fantasy.append(movie)
-                case "History":
-                    history.append(movie)
-                case "Horror":
-                    horror.append(movie)
-                case "Kids":
-                    kids.append(movie)
-                case "Music":
-                    music.append(movie)
-                case "Mystery":
-                    mystery.append(movie)
-                case "Reality":
-                    reality.append(movie)
-                case "Romance":
-                    romance.append(movie)
-                case "Science Fiction":
-                    scifi.append(movie)
-                case "Sci-Fi & Fantasy":
-                    scifi.append(movie)
-                    fantasy.append(movie)
-                case "Thriller":
-                    thriller.append(movie)
-                case "TV Movie":
-                    tv_movie.append(movie)
-                case "Music":
-                    music.append(movie)
-                case "War":
-                    war.append(movie)
-                case "War & Politics":
-                    war.append(movie)
-                case "Western":
-                    western.append(movie)
-                case _:
-                    print(f"\nUnmapped genre name: {genre.name}\n")
-
-    context = {
-        "action": action,
-        "adventure": adventure,
-        "animated": animated,
-        "comedy": comedy,
-        "crime": crime,
-        "documentary": documentary,
-        "drama": drama,
-        "family": family,
-        "fantasy": fantasy,
-        "history": history,
-        "horror": horror,
-        "kids": kids,
-        "music": music,
-        "mystery": mystery,
-        "reality": reality,
-        "romance": romance,
-        "scifi": scifi,
-        "thriller": thriller,
-        "tv_movie": tv_movie,
-        "war": war,
-    }
-    
-    return render(request, "djangoflix/view_content.html", context)
-
-
-def tv(request):
-    if not request.method == "GET":
-        return HttpResponseNotAllowed(["GET"])
-    try:
-        this_profile = Profile.get_one_profile_by_id(request.session["profile"])
-        if not this_profile:
-            raise Profile.DoesNotExist
-    # KeyError catches login skippers, DoesNotExist catches invalid session data
-    except (KeyError, Profile.DoesNotExist):
-        if not "account" in request.session:
-            # Send them to the login page, not register
-            # If they are trying to get to home, they probably have an account
-            return redirect(reverse_lazy("accounts:login"))
-        elif not "profile" in request.session:
-            return redirect(reverse_lazy("djangoflix:profiles"))
-        else:
-            return redirect(reverse_lazy("accounts:logout"))
-        
-    all_tv = WatchableContent.get_all_tv()
-    action = []
-    adventure = []
-    animated = []
-    comedy = []
-    crime = []
-    documentary = []
-    drama = []
-    family = []
-    fantasy = []
-    history = []
-    horror = []
-    kids = []
-    music = []
-    mystery = []
-    reality = []
-    romance = []
-    scifi = []
-    thriller = []
-    war = []
-    western = []
-
-    for tv in all_tv:
-        genres = tv.genres.all()
-        for genre in genres:
-            match genre.name:
-                case "Action":
-                    action.append(tv)
-                case "Action & Adventure":
-                    action.append(tv)
-                    adventure.append(tv)
-                case "Adventure":
-                    adventure.append(tv)
-                case "Animation":
-                    animated.append(tv)
-                case "Comedy":
-                    comedy.append(tv)
-                case "Crime":
-                    crime.append(tv)
-                case "Documentary":
-                    documentary.append(tv)
-                case "Drama":
-                    drama.append(tv)
-                case "Family":
-                    family.append(tv)
-                case "Fantasy":
-                    fantasy.append(tv)
-                case "History":
-                    history.append(tv)
-                case "Horror":
-                    horror.append(tv)
-                case "Kids":
-                    kids.append(tv)
-                case "Music":
-                    music.append(tv)
-                case "Mystery":
-                    mystery.append(tv)
-                case "Reality":
-                    reality.append(tv)
-                case "Romance":
-                    romance.append(tv)
-                case "Science Fiction":
-                    scifi.append(tv)
-                case "Sci-Fi & Fantasy":
-                    scifi.append(tv)
-                    fantasy.append(tv)
-                case "Thriller":
-                    thriller.append(tv)
-                case "Music":
-                    music.append(tv)
-                case "War":
-                    war.append(tv)
-                case "War & Politics":
-                    war.append(tv)
-                case "Western":
-                    western.append(tv)
-                case _:
-                    print(f"\nUnmapped genre name: {genre.name}\n")
-
-    context = {
-        "action": action,
-        "adventure": adventure,
-        "animated": animated,
-        "comedy": comedy,
-        "crime": crime,
-        "documentary": documentary,
-        "drama": drama,
-        "family": family,
-        "fantasy": fantasy,
-        "history": history,
-        "horror": horror,
-        "kids": kids,
-        "music": music,
-        "mystery": mystery,
-        "reality": reality,
-        "romance": romance,
-        "scifi": scifi,
-        "thriller": thriller,
-        "war": war,
-    }
-    
-    return render(request, "djangoflix/view_content.html", context)
-
-
-def favorites(request):
-    return HttpResponse("Hello, Favorites!")
-
-
-def search(request):
-    return HttpResponse("Hello, search!")
-
-
-def details(request):
-    return HttpResponse("Hello, Details!")
+    return context
